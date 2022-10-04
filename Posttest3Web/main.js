d2fa9a337cb8e7bfbb29961a73e8668e434fbfbb
Arsy Madya Budianto <arsymadyab@gmail.com>
2022-10-05 01:30:22 +0800

    posttest3web

---
+++ Posttest3Web/main.js
@@ -0,0 +1,10 @@
+function dlmode() {
+    var element = document.body;
+    element.classList.toggle("change-mode");
+}
+
+var judul = document.getElementById('judul')
+
+console.log(judul)
+
+judul.style.backgroundColor = "cyan"
